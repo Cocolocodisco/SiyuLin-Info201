4d105936260a4e910d55ab72075beeb952809cd2
changcynthia <axwukarmatic@gmail.com>
2022-11-07 12:01:58 -0800

    source code map/chart 1 creation

---
+++ source/map_chart1.R
@@ -0,0 +1,5 @@
+# this is the source code for a map detailing a sample of 50000 fires
+# that have happened in the United States from 1992-2015
+# by Cynthia Chang
+
+fire_data <- read.csv("~info201/project-group-7-section-af")
