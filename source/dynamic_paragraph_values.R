8bcc5e3bf3f43d3f5c7db42b8441c48ecf7c2fdc
changcynthia <axwukarmatic@gmail.com>
2022-11-14 15:37:30 -0800

    source 2 code written as well as dynamic paragraph

--- source/dynamic_paragraph_values.R
+++ source/dynamic_paragraph_values.R
@@ -7,21 +7,28 @@ fire_data <- read.csv("../data/FW_Veg_Rem_Combined.csv")
 us_temp_data <- read.csv("../data/united_states.csv")
 acres_burned_data <- read.csv("../data/acres_burned_in_wildfires_usafacts.csv")
 
-testing <- as.data.frame(table(fire_data$stat_cause_descr)) %>% 
+most_common_cause <- as.data.frame(table(fire_data$stat_cause_descr)) %>% 
   filter(Freq == max(Freq)) %>% 
   pull(Var1)
-
-most_common_cause <- function() {
-  causes <- unique(fire_data$stat_cause_descr)
-  answer <- causes[which.max(tabulate(match(fire_data, causes)))]
-  return(answer)
-}
   
-# most_common_state <- as.data.frame(table(fire_data$state)) %>%
-#  filter(Freq) %>%
+most_common_state <- as.data.frame(table(fire_data$state)) %>%
+  filter(Freq == max(Freq)) %>%
+  pull(Var1)
   
+most_discovery_month <- as.data.frame(table(fire_data$discovery_month)) %>%
+  filter(Freq == max(Freq)) %>%
+  pull(Var1)
 
+highest_average_temperature <- us_temp_data %>%
+  group_by(State) %>%
+  summarize(avg = mean(AvgTemperature)) %>%
+  ungroup() %>%
+  filter(avg == max(avg)) %>%
+  pull(State, avg)
 
+where_is_texas <- us_temp_data %>%
+  filter(State == "Texas") %>%
+  summarize(average = mean(AvgTemperature))
 
 # reference: https://www.statology.org/r-count-number-of-occurrences-in-column/
 # reference: https://bookdown.org/kdonovan125/ibis_data_analysis_r4/working-with-tables-in-r.html
