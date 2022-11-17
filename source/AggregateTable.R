93db3b83569819c7bfb5d4bd1669fa8bf0998918
ylc22 <ylc22@uw.edu>
2022-11-17 00:19:47 -0800

    done

--- source/AggregateTable.R
+++ source/AggregateTable.R
@@ -1,28 +1,41 @@
 library(tidyverse, warn.conflicts = FALSE)
-library(dplyr,warn.conflicts = FALSE)
-library(tidyr,warn.conflicts = FALSE)
+library(dplyr, warn.conflicts = FALSE)
+library(tidyr, warn.conflicts = FALSE)
 library(data.table,warn.conflicts = FALSE)
 
-
+#reads the data file
 wildfire <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-7-section-af/main/data/FW_Veg_Rem_Combined.csv")
+aggregate_table <- wildfire %>%
+  mutate (disc_clean_date, year = format (as.Date (wildfire$disc_clean_date,
+                                                    format="%m/%d/%Y"), "%Y")) %>%
+  select(year, state, stat_cause_descr, discovery_month)
+test <- sort(as.numeric(unique (aggregate_table$year)))
 
-most_common_cause <- as.data.frame(table(wildfire$stat_cause_descr)) %>% 
-  group_by(wildfire$disc_pre_year) %>%
-  filter(Freq == max(Freq)) %>% 
-  pull(Var1)
+#filters the data to get the state with the most wildfires
+max_state <- aggregate_table %>%
+  select(year, state) %>%
+  group_by(year) %>%
+  count (state) %>%
+  filter (n == max(n)) %>%
+  pull (state)
 
-most_common_state <- as.data.frame(table(wildfire$state)) %>%
-  group_by(wildfire$disc_pre_year) %>%
-  filter(Freq == max(Freq)) %>%
-  pull(Var1)
+#filters the data to get the mnonth with the most wildfires
+max_discovery_month <- aggregate_table %>%
+  select(year, discovery_month) %>%
+  group_by(year) %>%
+  count(discovery_month) %>%
+  filter (n== max(n)) %>%
+  pull(discovery_month)
 
-most_discovery_month <- as.data.frame(table(wildfire$discovery_month)) %>%
-  group_by(wildfire$disc_pre_year) %>%
-  filter(Freq == max(Freq)) %>%
-  pull(Var1)
-  
-AggregateDataTable <- merge(most_common_cause,most_common_state,most_discovery_month) 
+#filters the data to get the most listed reason of the wildfires
+max_stat_cause_descr<- aggregate_table %>%
+  select(year, stat_cause_descr) %>%
+  group_by(year) %>%
+  count(stat_cause_descr) %>%
+  filter (n == max(n)) %>%
+  pull (stat_cause_descr)
 
-AggregateDataTable <- knitr::kable(AggregateDataTable, "simple", col.names = c("Year", "Most caused by reasons", "State that had the most", "Months that had the most"))
-
-
+#combines the data warngled and makes the table
+combine_tables <- data.frame(test, max_state, max_stat_cause_descr, max_discovery_month)
+print_aggregate_table <- knitr::kable(combine_tables, "simple",
+                                      kol.names = c("Year","State", "Cause of Fire", "Month it was discovered"))
