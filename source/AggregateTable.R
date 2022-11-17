afb7f905a662c77d6dd54751eb9efbba55d70494
ylc22 <ylc22@uw.edu>
2022-11-16 21:47:07 -0800

    done

--- source/AggregateTable.R
+++ source/AggregateTable.R
@@ -6,31 +6,23 @@ library(data.table,warn.conflicts = FALSE)
 
 wildfire <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-7-section-af/main/data/FW_Veg_Rem_Combined.csv")
 
-aggregate_table_2 <- data.table(wildfire) %>%
-  select(disc_clean_date, state, stat_cause_descr, fire_mag, Temp_pre_30, Prec_pre_15) %>%
-  filter(disc_clean_date == "1/1/1992" | disc_clean_date == "1/1/1995" | disc_clean_date == "1/1/1998" | disc_clean_date == "1/1/2001" | disc_clean_date == "1/1/2004" | disc_clean_date == "1/1/2007" | disc_clean_date == "1/1/2010" | disc_clean_date == "1/1/2013" | disc_clean_date == "9/9/2015")
+most_common_cause <- as.data.frame(table(wildfire$stat_cause_descr)) %>% 
+  group_by(wildfire$disc_pre_year) %>%
+  filter(Freq == max(Freq)) %>% 
+  pull(Var1)
+
+most_common_state <- as.data.frame(table(wildfire$state)) %>%
+  group_by(wildfire$disc_pre_year) %>%
+  filter(Freq == max(Freq)) %>%
+  pull(Var1)
+
+most_discovery_month <- as.data.frame(table(wildfire$discovery_month)) %>%
+  group_by(wildfire$disc_pre_year) %>%
+  filter(Freq == max(Freq)) %>%
+  pull(Var1)
+  
+AggregateDataTable <- merge(most_common_cause,most_common_state,most_discovery_month) 
+
+AggregateDataTable <- knitr::kable(AggregateDataTable, "simple", col.names = c("Year", "Most caused by reasons", "State that had the most", "Months that had the most"))
 
 
-
-MeanTemp <- aggregate_table_2 %>%
-  group_by(Temp_pre_30) %>%
-  group_by(stat_cause_descr) %>%
-  summarise(Temp_pre_30 = mean(Temp_pre_30))
-
-MeanPrec <- aggregate_table_2 %>%
-  group_by(Prec_pre_15) %>%
-  group_by(stat_cause_descr) %>%
-  summarise(Prec_pre_15 = mean(Prec_pre_15))
-
-MeanMagnitude <- aggregate_table_2 %>%
-  group_by(fire_mag) %>%
-  group_by(stat_cause_descr) %>%
-  summarise(fire_mag = mean(fire_mag))
-
-
-AggData <- merge(MeanTemp, MeanPrec)
-AggregateDataTable <- merge(AggData, MeanMagnitude) 
-
-AggregateDataTable2 <- knitr::kable(AggregateDataTable, "simple", col.names = c("Cause","Temperature","Precipitation","Fire Maginitude"))
-
-            
