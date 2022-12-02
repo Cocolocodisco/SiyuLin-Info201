6a3dcdad1c1f49659e320fbbf1389559ec7b6f97
changcynthia <axwukarmatic@gmail.com>
2022-12-01 16:53:50 -0800

    added all the code for map

--- source/wildfireproject/server.R
+++ source/wildfireproject/server.R
@@ -7,13 +7,36 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-library(maps)
 
-source("../map_chart2.R")
+#source("../map_chart2.R")
 
 
 # testing map data shenanigans
+library(shiny)
+library(tidyverse)
+library(leaflet)
+library(maps)
+
+
+fire_data <- read.csv("../../data/FW_Veg_Rem_Combined.csv")
+
+ph_location_density <-fire_data %>% select(state, disc_clean_date, stat_cause_descr,
+                                           latitude, longitude)
+location_density <- fire_data %>% select(state, disc_clean_date) %>% 
+  mutate(disc_clean_date, year = format(as.Date(ph_location_density$disc_clean_date, 
+                                                format="%m/%d/%Y"),"%Y"))
+
+#max(count_state$n)
+# this counts the number of fires that each state has had per year
+count_by_cause <- ph_location_density %>%
+  group_by(stat_cause_descr) %>%
+  count(state) %>%
+  ungroup()
+
+new_count_cause <- count_by_cause %>%
+  mutate(region = tolower(state.name[match(state, state.abb)]))
+
+# basic us map reference
 map_data <- map_data("state")
 map <- ggplot() + 
   geom_polygon(data = us_map, aes(x=long, y=lat, group=group),
